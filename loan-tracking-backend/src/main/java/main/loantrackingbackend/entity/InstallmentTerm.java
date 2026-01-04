742f26c34fad0a049d2693f2cef2343801f4f4b0
Gichelle Burwell <gcburwell@up.edu.ph>
2026-01-05 07:29:03 +0800

    Removed autocompute because it is already handled

--- loan-tracking-backend/src/main/java/main/loantrackingbackend/entity/InstallmentTerm.java
+++ loan-tracking-backend/src/main/java/main/loantrackingbackend/entity/InstallmentTerm.java
@@ -6,12 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import main.loantrackingbackend.enums.InstallmentStatus;
-import main.loantrackingbackend.enums.PaymentFrequency;
 import main.loantrackingbackend.util.TestDateManager;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.temporal.TemporalAdjusters;
 
 @Entity
 @Table(name = "installment_term")
@@ -43,42 +40,5 @@ public class InstallmentTerm {
         if(paid) return InstallmentStatus.PAID;
         if(TestDateManager.today().isAfter(dueDate)) return InstallmentStatus.DELINQUENT;
         return InstallmentStatus.UNPAID;
-    }
-
-    /* useful but unsure where to use methods
-    public BigDecimal getExpectedAmountPaid() {
-        return installmentExpense.getPaymentAmountPerTerm().multiply(BigDecimal.valueOf(termNumber));
-    }
-
-    public BigDecimal getAmountPaid() {
-        if (installmentExpense == null || installmentExpense.getPayments() == null) {
-            return BigDecimal.ZERO;
-        }
-
-        return installmentExpense.getPayments()
-                .stream()
-                .filter(payment -> payment.getPayee().getPersonId().equals(installmentExpense.getPersonBorrower().getPersonId()))
-                .map(Payment::getPaymentAmount)
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
-    } */
-
-    public void computeDueDate() {
-        LocalDate startDate = installmentExpense.getStartDate();
-        PaymentFrequency frequency = installmentExpense.getPaymentFrequency();
-        DayOfWeek weeklyPayDay = installmentExpense.getWeeklyPayDay();
-
-        if (frequency == PaymentFrequency.MONTHLY) {
-            dueDate = startDate.plusMonths(termNumber - 1);
-
-        } else if (frequency == PaymentFrequency.WEEKLY) {
-            LocalDate tempDate = startDate;
-            tempDate = tempDate.with(TemporalAdjusters.nextOrSame(weeklyPayDay));
-            dueDate = tempDate.plusWeeks(termNumber - 1);
-        }
-    }
-
-    public LocalDate getDueDate() {
-        if (dueDate == null) computeDueDate();
-        return dueDate;
     }
 }
